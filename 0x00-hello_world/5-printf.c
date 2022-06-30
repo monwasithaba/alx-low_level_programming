58389f10ed9a714ab2d5cb63562601020b34bf46
root <msithaba@gmail.com>
2022-06-30 14:46:38 -0700

    commit the last three

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+*main - prints the size of various types
+*Return 0 if executed correctly
+*/
+
+int main(void)
+{
+printnf("with grammar, but the outcome is a place of art,\n");
+return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <studio.h>
+
+/**
+*main - prints the size of various types
+*Return 0 if executed correctly
+*/
+
+int main(void)
+{
+printnf("Size of a char: %d byte(s)\n", sizeof(char));
+printnf("Size of a int: %d byte(s)\n", sizeof(int));
+printnf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printnf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printnf("Size of a float: %d byte(s)\n", sizeof(float));
+return(0);
+}
