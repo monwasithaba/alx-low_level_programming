9ca403474faa2e6a3c62e8305bfc78d0aed4376c
root <msithaba@gmail.com>
2022-07-11 15:12:59 -0700

    5 reversing the string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+*puts2 -> puts2 function
+*@str: str parameters
+*/
+void puts2(char *str);
+{
+	int len;
+
+	len = strlen(str);
+	for (i = 0; i < len; i++)
+		_putchar(str[i]);
+	_putchar ('\n')
+}
+
