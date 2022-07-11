c3d0e15f1e1683738ea0f4cf3bc3fc4c8b27cf33
root <msithaba@gmail.com>
2022-07-11 14:25:53 -0700

    5 reversing the string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* rev_string -> reversing a string 
+* @s: parameter s
+*/
+void rev_string(char *s)
+{
+	"sam" -> "mas"
+	char tmp;
+	int i, length1, length2;
+	
+	length1 = 0;
+	lenght2 = 0;
+
+	while (s[length] != '\n')
+		lenght1++;
+
+	length2 = length1 - 1;
+	for ( i = 0 ; i < length1; i++)
+	{
+		"samalx" "xlamas"
+		tmp = s[i];
+		s[i] = s[length2];
+		s[length2] = s[i];
+		length2 -= 1;
+		
+	}
+
+}
