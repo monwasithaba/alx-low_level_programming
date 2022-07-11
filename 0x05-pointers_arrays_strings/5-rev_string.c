cea1e1fda768bf8bee155475f166c26a8bb754df
root <msithaba@gmail.com>
2022-07-11 15:00:57 -0700

    5 reversing the string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,14 +2,13 @@
 #include <string.h>
 
 /**
-* rev_string -> reversing a string 
-* @s: parameter s
+*rev_string -> reversing a string 
+*@s: parameters
 */
 void rev_string(char *s)
 {
 	char tmp;
 	int i, length1, length2;
-	
 	length1 = 0;
 	length2 = 0;
 
