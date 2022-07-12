321e640867106cd1fbc7630b4a6fc232462baf3f
root <msithaba@gmail.com>
2022-07-11 19:19:09 -0700

    7 task

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,12 +9,12 @@ void puts_half(char *str)
 {
 	int x, y;
 
-	x = strlen(str)
+	x = strlen(str);
 	if (x % 2 == 1)
 		y = x / 2 = 1;
 	else
 		y = x / 2;
 	for (i = y; i < x; i++)
-		_putchar(str[i];
+		_putchar(str[i]);
 	_putchar('\n');
 }
