0f1141c9f30c9ca22f2b29e36c9b8c58f27a4a6c
root <msithaba@gmail.com>
2022-07-14 07:07:13 -0700

    commit task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,13 +3,13 @@
 
 /**
 *_strncat -> function to append some charx
-*@dest: first param
-*@src: second param
-*@n: third param
-*Return: string
+* @dest: first param
+* @src: second param
+* @n: third param
+* Return: string
 */
 char *_strncat(char *dest, char *src, int n)
 {
-	strncat(dest, src, n)
+	strncat(dest, src, n);
 	return (dest);
 }
