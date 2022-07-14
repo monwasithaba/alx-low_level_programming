c2414159da0584768c6fba502a17cffbd2960f6a
root <msithaba@gmail.com>
2022-07-14 08:53:07 -0700

    commit task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+* _strncmp -> for copying purpose
+* @s1: parameter 1
+*  @s2: parameter 2
+*  Return: integer
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+	int result;
+	
+	result = strcmp(s1, s2);
+	return (result);
+}
