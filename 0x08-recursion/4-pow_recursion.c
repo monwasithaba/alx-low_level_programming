b6a251764fd881744ae781596b28e125ad5e6260
root <msithaba@gmail.com>
2022-07-21 16:51:54 -0700

    commit task 4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,8 +4,8 @@
 *@x: number
 *@y: power
 *
-*Return: factorial of number
-*-1 if x < 0
+*Return: x to the power of y
+* -1 if x < 0
 */
 int _pow_recursion(int x, int y)
 {
@@ -15,5 +15,5 @@ int _pow_recursion(int x, int y)
 		return (1);
 	if (y == 1)
 		return (x);
-	return (n * _pow_recursion(x, y - 1));
+	return (x * _pow_recursion(x, y - 1));
 }
